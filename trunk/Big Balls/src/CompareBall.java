17bbe27c101cdcd88474dd398ac139664758d328
marianocamarzana <marianocamarzana@d6e35a34-a1ea-11dd-adfd-a1ec4d499d49>
2008-11-07 18:43:53 +0000

    Soporte para el ordenamiento del Vector orderedballs

--- trunk/Big Balls/src/CompareBall.java
+++ trunk/Big Balls/src/CompareBall.java
@@ -1,19 +1,25 @@
 
 import java.util.Comparator;
 
+/**
+ * Me provee soporte para realizar la comparacion para el posterior
+ * prdenamiento.
+ * 
+ */
+public class CompareBall  implements Comparator {
 
-public class CompareBall  implements Comparator{
-
-
+/**
+ * 
+ */
 	public CompareBall() {
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	/**
+	 * Compara por el valor del objeto en forma ascendente.
 	 */
-	@Override
 	public int compare(Object o1, Object o2) {
-		return new Integer(((Ball)o1).getValue()).compareTo (new Integer(((Ball)o2).getValue()));
+		return new Integer(((Ball) o1).getValue()).
+		compareTo(new Integer(((Ball) o2).getValue()));
 	}
 
 }
