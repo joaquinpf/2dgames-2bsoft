8e5904fc6c5775c981b70fc3b88761c6f39fbe5a
silvinawesner <silvinawesner@d6e35a34-a1ea-11dd-adfd-a1ec4d499d49>
2008-11-08 23:47:19 +0000

    Clase encargada de la generación del siguiente nivel del juego.

--- trunk/Memo/src/LevelGenerator.java
+++ trunk/Memo/src/LevelGenerator.java
@@ -1,13 +1,36 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Attribute;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 import com.golden.gamedev.GameEngine;
+import com.golden.gamedev.engine.BaseIO;
+import com.golden.gamedev.engine.BaseLoader;
+import com.golden.gamedev.object.Background;
+import com.golden.gamedev.object.background.ImageBackground;
 
 
-
+/**
+ * Clase encargada de la administración de los niveles del juego.
+ */
 public class LevelGenerator {
 
 	/**
 	 * @uml.property  name="configRoute"
 	 */
 	private String configRoute = "";
+	
+	/**
+	 * @uml.property  name="bsLoader"
+	 */
+	private BaseLoader bsLoader;
 
 	/**
 	 * Getter of the property <tt>configRoute</tt>
@@ -25,22 +48,72 @@ public class LevelGenerator {
 	 */
 	public void setConfigRoute(String configRoute) {
 		this.configRoute = configRoute;
+		  this.bsLoader = new BaseLoader(new BaseIO(this.getClass()), 
+				  Color.MAGENTA);
 	}
 
 
 	/**
+	 * Constructor de la clase
 	 */
-	public LevelGenerator(String _route){
+	public LevelGenerator(String _route) {
+		this.configRoute = _route;
+	}
+	
+	/**
+	 * @return  BufferedImage
+	 */
+	private BufferedImage getImage(String imagefile) {
+         return this.bsLoader.getImage(imagefile);
 	}
 
-
-		
-		/**
-		 */
-		public Level generateLevel(GameEngine parent){
-		
-				return null;
-			 }
+	/**
+	 * Este método genera el siguiente nivel al que avanza el juego. 
+	 * Si el archivo xml de configuración del juego es válido retorna el 
+	 * siguiente nivel, en caso contrario retorna null 
+	 * @return  Level
+	 */
+	public Level generateLevel(GameEngine parent) {
+		int cantidadPares = 0;
+		Level level = new Level(parent);
+		this.currentLevel += 1;
+	        
+		SAXBuilder builder = new SAXBuilder(true);
+			
+        try {
+	        Document doc = builder.build(new File(this.getConfigRoute()));
+	        Element root = doc.getRootElement();  
+		    List child = root.getChildren();
+		    List childrensConfig = ((Element) child.get(1)).getChildren();                        
+		    List childrensLevels = ((Element) childrensConfig.get(0)).getChildren();
+		    List childrensCards =  ((Element) childrensConfig.get(1)).getChildren();  
+		    for (Iterator iterator = childrensLevels.iterator();
+		    	 iterator.hasNext();) {	
+		      Element tagLevel = (Element) iterator.next();
+		      if ((tagLevel.getAttribute("ID").getIntValue() == this.currentLevel)) {
+		    	  level.setRemainingTimeLevel(tagLevel.getAttribute("time").getIntValue());
+		    	  cantidadPares = tagLevel.getAttribute("pairs").getIntValue();
+		    	  level.setTableSize(cantidadPares * 2);
+		    	  Background background = new ImageBackground(this.getImage(tagLevel.getAttribute("backGround").getValue())); 			
+                  level.setBackground(background);
+  		          level.setLevelNumber(this.currentLevel);
+  		      }
+		    }
+		    for (int i = 1; i <= cantidadPares; i++) {
+		    	 Element tagCard = (Element) childrensCards.get(i);
+		    	 Card card = new Card();
+	             card.setImage(this.getImage(tagCard.getAttribute("images").getValue()));  
+	             level.addCard(card);
+		    }
+		    return level;
+		} catch (JDOMException jdomException) {
+	        System.out.println("Documento XML mal formado o incorrecto.");
+	        jdomException.printStackTrace();
+	    } catch (Exception exception) {
+	    	exception.printStackTrace();
+	    }
+	    return null;
+	}
 
 
 	/**
@@ -65,5 +138,4 @@ public class LevelGenerator {
 	public void setCurrentLevel(int currentLevel) {
 		this.currentLevel = currentLevel;
 	}
-
 }
