e616be54eaf7ba8b968d951f6aa5b39677c85f65
marianocamarzana <marianocamarzana@d6e35a34-a1ea-11dd-adfd-a1ec4d499d49>
2008-11-10 14:46:26 +0000

    Proyecto Big Balls:->Creacion de la clase Level

--- trunk/Big Balls/src/Level.java
+++ trunk/Big Balls/src/Level.java
@@ -1,19 +1,26 @@
-
-
 import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameObject;
+import com.golden.gamedev.object.Background;
+import com.golden.gamedev.object.GameFont;
 import com.golden.gamedev.object.PlayField;
+import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.background.ImageBackground;
 
 import java.awt.Graphics2D;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Vector;
-import java.util.Collection;
+
 /**
+ * @uml.dependency supplier="Ball"
+ */
+/**
+ * @author M&M
  * 
  */
-
-
 public class Level extends GameObject implements Observer {
 
 	/**
@@ -21,217 +28,192 @@ public class Level extends GameObject implements Observer {
 	 */
 	public Level(GameEngine parent) {
 		super(parent);
-		// TODO Auto-generated constructor stub
+		this.orderedBalls = new Vector<Ball>();
 	}
 
 	/**
-	 * @uml.property  name="levelComplete"
+	 * @uml.property name="levelComplete"
 	 */
 	private boolean levelComplete = false;
 
 	/**
-	 * Getter of the property <tt>levelComplete</tt>
-	 * @return  Returns the levelComplete.
-	 * @uml.property  name="levelComplete"
-	 */
-	public boolean isLevelComplete() {
-		return levelComplete;
-	}
-
-	/**
-	 * Setter of the property <tt>levelComplete</tt>
-	 * @param levelComplete  The levelComplete to set.
-	 * @uml.property  name="levelComplete"
-	 */
-	public void setLevelComplete(boolean levelComplete) {
-		this.levelComplete = levelComplete;
-	}
-
-	/**
-	 * @uml.property  name="playfield"
+	 * @uml.property name="playfield"
 	 */
 	private PlayField playfield;
 
 	/**
-	 * Getter of the property <tt>playfield</tt>
-	 * @return  Returns the playfield.
-	 * @uml.property  name="playfield"
-	 */
-	public PlayField getPlayfield() {
-		return playfield;
-	}
-
-	/**
-	 * Setter of the property <tt>playfield</tt>
-	 * @param playfield  The playfield to set.
-	 * @uml.property  name="playfield"
-	 */
-	public void setPlayfield(PlayField playfield) {
-		this.playfield = playfield;
-	}
-
-	/** 
 	 * @uml.property name="orderedBalls"
 	 */
-	private Vector <Ball> orderedBalls;
+	private Vector<Ball> orderedBalls;
 
-	/** 
-	 * Getter of the property <tt>orderedBalls</tt>
-	 * @return  Returns the orderedBalls.
-	 * @uml.property  name="orderedBalls"
+	/**
+	 * Getter of the property <tt>orderedBalls</tt>.
+	 * 
+	 * @return Returns the orderedBalls.
+	 * @uml.property name="orderedBalls"
 	 */
-	public Vector <Ball> getOrderedBalls() {
+	public Vector<Ball> getOrderedBalls() {
 		return orderedBalls;
 	}
 
-	/** 
-	 * Setter of the property <tt>orderedBalls</tt>
-	 * @param orderedBalls  The orderedBalls to set.
-	 * @uml.property  name="orderedBalls"
+	/**
+	 * @uml.property name="levelNumber"
 	 */
-	public void setOrderedBalls(Vector <Ball> orderedBalls) {
-		this.orderedBalls = orderedBalls;
-	}
+	private static int levelNumber;
 
 	/**
-	 * @uml.property  name="levelNumber"
-	 */
-	private int levelNumber;
-
-	/**
-	 * Getter of the property <tt>levelNumber</tt>
-	 * @return  Returns the levelNumber.
-	 * @uml.property  name="levelNumber"
+	 * Getter of the property <tt>levelNumber</tt>.
+	 * 
+	 * @return Returns the levelNumber.
+	 * @uml.property name="levelNumber"
 	 */
 	public int getLevelNumber() {
 		return levelNumber;
 	}
 
 	/**
-	 * Setter of the property <tt>levelNumber</tt>
-	 * @param levelNumber  The levelNumber to set.
-	 * @uml.property  name="levelNumber"
+	 * Setter of the property <tt>levelNumber</tt>.
+	 * 
+	 * @param levelNumber
+	 *            The levelNumber to set.
+	 * @uml.property name="levelNumber"
 	 */
 	public void setLevelNumber(int levelNumber) {
 		this.levelNumber = levelNumber;
 	}
 
+	/**
+	 * Se seleciono ordenar el Vector orderedBalls empleando la interfaz
+	 * Comparator, ya que de esta manera se puede extender la forma de
+	 * realizar dicho ordenamiento.
+	 */
+	public void addBall(Ball newBall, Comparator c) {
+		orderedBalls.add(newBall);
+		Collections.sort(orderedBalls, c);
+	}
 
 	/**
-	 */
-	public void addBall(Ball newBall){
-	}
-
-
-	/**
-	 */
-	public void update(){
-	}
-
-	/** 
-	 * @uml.property name="ball"
-	 * @uml.associationEnd multiplicity="(0 -1)" inverse="level:Ball"
-	 */
-	private Collection ball;
-
-	/** 
-	 * Getter of the property <tt>ball</tt>
-	 * @return  Returns the ball.
-	 * @uml.property  name="ball"
-	 */
-	public Collection getBall() {
-		return ball;
-	}
-
-	/** 
-	 * Setter of the property <tt>ball</tt>
-	 * @param ball  The ball to set.
-	 * @uml.property  name="ball"
-	 */
-	public void setBall(Collection ball) {
-		this.ball = ball;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 * @param o
+	 *            .
+	 * @param arg
+	 *            Cuando recibe el evento de que sucedio un cambio da por
+	 *            finalizado el juego
 	 */
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-
+		this.finish(); // Cuando expira el tiempo finalizar el nivel
 	}
 
 	/**
-	 * @uml.property  name="clock"
+	 * Timer para controlar el tiempo de la partida.
 	 */
 	private Clock clock;
 
 	/**
-	 * Getter of the property <tt>clock</tt>
-	 * @return  Returns the clock.
-	 * @uml.property  name="clock"
+	 * variable para llevar la posicion de la pelota seleccionada y compararla.
+	 * con el arreglo orderedBalss
 	 */
-	public Clock getClock() {
-		return clock;
+	private int pos;
+
+	/**
+	 *Empleado para manejar las colisiones entre las pelotas (sprites).
+	 */
+	private BallCollision ballcollision;
+
+	/**
+	 * Empleado para manejar las colisiones de los sprites y los limites de la
+	 * pantalla.
+	 */
+	private BorderCollision bordercollision;
+
+	/**
+	 * Variable empleada para agrupar las pelotas.
+	 */
+	private SpriteGroup BALLGROUP;
+
+	/**
+	 * 
+	 */
+	private SpriteGroup BACKGROUND;
+
+	/**
+	 * 
+	 */
+	private Background background;
+
+	/**
+	 * Para realizar los mensajes por pantalla.
+	 */
+	private GameFont font;
+
+	/**
+	 * Metodo de inicializacion de las variables involucradas.
+	 */
+	public void initResources() {
+		this.pos = 0;
+		this.clock = new Clock();
+		this.clock.setTotalTime("200");
+		this.clock.start();
+		this.clock.addObserver(this);
+		this.playfield = new PlayField();
+		background = new ImageBackground(getImage("resources/background.jpg"),
+				640, 480);
+		playfield.setBackground(background);
+		this.BALLGROUP = new SpriteGroup("balls");
+		this.BALLGROUP = new SpriteGroup("BACKGROUND");
+		this.playfield.addGroup(this.BALLGROUP);
+		for (Enumeration<Ball> e = this.orderedBalls.elements(); e
+		.hasMoreElements();) {
+			Ball ball = e.nextElement();
+			this.playfield.add(ball);
+			this.BALLGROUP.add(ball);
+		}
+		bordercollision = new BorderCollision(this.background);
+		this.playfield.addCollisionGroup(this.BALLGROUP, this.BACKGROUND,
+				this.bordercollision);
+
+		font = fontManager.getFont(getImages("resources/font.png", 20, 3),
+				" !            .,0123" + "456789:   -? ABCDEFG"
+				+ "HIJKLMNOPQRSTUVWXYZ ");
+
 	}
 
 	/**
-	 * Setter of the property <tt>clock</tt>
-	 * @param clock  The clock to set.
-	 * @uml.property  name="clock"
+	 * @param g
+	 *            Renderiza el juego en la pantalla.
+	 */
+	public void render(Graphics2D g) {
+		playfield.render(g);
+		font.drawString(g,
+				"LEVEL :" + new Integer(this.levelNumber).toString(), 10, 10);
+	}
+
+	/**
+	 * Actualiza las variables del juego. Controla ls pelotas que va
+	 * seleccionando el jugador, en caso de equivaocarse da por finalizado el
+	 * juego.
+	 */
+
+	public void update(long elapsedTime) {
+		playfield.update(elapsedTime);
+		if (click()) {
+			Ball ballsel = (Ball) this.checkPosMouse(this.playfield, true);
+			if (ballsel != null) {
+				if (ballsel.getValue() == this.orderedBalls.elementAt(pos)
+						.getValue()) {
+					ballsel.setActive(false);
+					this.pos++;
+				} else {
+					this.finish();
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param clock2
 	 */
 	public void setClock(Clock clock) {
 		this.clock = clock;
+
 	}
-
-	/* (non-Javadoc)
-	 * @see com.golden.gamedev.GameObject#initResources()
-	 */
-	@Override
-	public void initResources() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see com.golden.gamedev.GameObject#render(java.awt.Graphics2D)
-	 */
-	@Override
-	public void render(Graphics2D arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see com.golden.gamedev.GameObject#update(long)
-	 */
-	@Override
-	public void update(long arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * @uml.property  name="gameplay"
-	 * @uml.associationEnd  inverse="level:Gameplay"
-	 */
-	private Gameplay gameplay;
-
-	/**
-	 * Getter of the property <tt>gameplay</tt>
-	 * @return  Returns the gameplay.
-	 * @uml.property  name="gameplay"
-	 */
-	public Gameplay getGameplay() {
-		return gameplay;
-	}
-
-	/**
-	 * Setter of the property <tt>gameplay</tt>
-	 * @param gameplay  The gameplay to set.
-	 * @uml.property  name="gameplay"
-	 */
-	public void setGameplay(Gameplay gameplay) {
-		this.gameplay = gameplay;
-	}
-
 }
