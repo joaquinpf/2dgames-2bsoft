5eb01caf5b233cbc9cd8a0ad0ae754645c3413f5
joaquinpf <joaquinpf@d6e35a34-a1ea-11dd-adfd-a1ec4d499d49>
2009-02-10 14:16:50 +0000

    Spots: Primer version de los spots informativos. Se incluye un ejemplo y clases de prueba en el paquete *.test

---
+++ trunk/Spots/src/ar/edu/unicen/exa/spots/test/ActionAceptar.java
@@ -0,0 +1,12 @@
+package ar.edu.unicen.exa.spots.test;
+import java.awt.event.*;
+
+public class ActionAceptar implements ActionListener {
+	public ActionAceptar() {
+			
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Aceptar");		
+	}	
+}
---
+++ trunk/Spots/src/ar/edu/unicen/exa/spots/test/ActionCancelar.java
@@ -0,0 +1,12 @@
+package ar.edu.unicen.exa.spots.test;
+import java.awt.event.*;
+
+public class ActionCancelar implements ActionListener {
+	public ActionCancelar() {
+			
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Cancelar");		
+	}	
+}
